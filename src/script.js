29d844ea1ac76a30651f3f770620d2b280fff4b7
brld <brandon.lee.darby@gmail.com>
2020-08-15 16:56:49 -0700

    Revamp website with features such as updated blurbs, acca trophy hall, updated partners page, complete discography

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function toggle() {
+    var iframe = document.getElementById("interactiveIframe");
+    var button = document.getElementById("interactiveButton");
+    iframe.classList.remove("hidden");
+    button.classList.add("hidden");
+}
