1b72770e566b2791d9eeb44eaf2c8f0bee834146
brld <brandon.lee.darby@gmail.com>
2020-12-31 02:56:08 -0800

    Update ACCAs page with December ACCAs information, create dropdowns to toggle between June and December ACCA displays, update ACCA grid spacing to allow for longer link descriptions, update home page with December ACCA logo

--- src/script.js
+++ src/script.js
@@ -4,3 +4,18 @@ function toggle() {
     iframe.classList.remove("hidden");
     button.classList.add("hidden");
 }
+
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.parentNode.className = 'accordionItem open';
+    }
+}
